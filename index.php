ba2293404c9049961b6d8e31c369897009ab7266
Rickymero <sebetha.ricky@gmail.com>
2022-03-23 10:52:24 +0200

    New files

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link  rel="stylesheet" type= "text/css" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rammetloane Enterprise PTY</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="images/Rammetloane logo.png" id="logo"> 
+            <h1 id="head">RAMMETLOANE ENTERPRISE PTY       
+                <li>
+                    <ul><a href="">About</a></ul>
+                    <ul><a href="">Services</a></ul>
+                    <ul><a href="">Directions</a></ul>
+                    <ul><a href="">Gallary</a></ul>
+                </li>
+            </h1>
+        </nav>
+    </header>
+    <section>
+        <div class="wrapper">
+            <div class="content">
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                <br><br><br><br><br><br><br><br>
+                <h1 id="welcome">Welcome To Rammetloane Enterprise PTY</h1>
+                <h3 id="slogan">Your one stop for Home and Business solutions</h3>
+            </div>
+        </div>
+    </section>
+    <h2 id="ourservice">About US</h2>
+    <div class="about">
+        <div class="box1">
+            <p style="padding-top: 20px;
+                    padding-left: 40px;
+                    padding-right: 40px; 
+                    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                    color:rgb(60, 114, 116);
+                    font-size: 22px;
+                    text-align: center;">
+                Rammetloane Enterprise was established in 2020 during covid-19 break out.
+                Our core business is manufacturing, supply, and erection of Shadeports and
+                Carports used for the protection of the motor vehicles against Sun and Hail
+                as well as shade net used in the agriculture sector. We also do maintenance 
+                on existing shadeports. Shadeport Systems can also provide Engineering 
+                Certificates and Drawings when needed. Our other side of business is Blinds
+                Installation and Maintenance. From timeless venetian blinds to modern double
+                roller blinds. At interior blinds we pride ourselves on being the best blinds
+                supplier in Johannesburg. We supply quality blinds and window accessories.
+                All our stuff is highly trained and qualified in power tools and working at
+                heights as well as building of scaffolding on site.
+            </p>
+        </div>
+        <div id="box1">
+            <img id ="owner" src="images/owner.jpg" height="230px" width="350px" style="background-size: cover;">
+        </div>
+        <div id="box1">
+            <p style="
+                    padding-top: 20px;
+                    padding-left: 40px;
+                    padding-right: 40px;
+                    text-align: center;
+                    font-size: 14px;
+                    color: black;">
+                Did You Know: Interior shutters fit snugly to your windows and have slats
+                called louvers. Most louvers can be closed completely or slanted to let in
+                filtered light.
+            </p>
+        </div>
+    </div>
+   
+    <h2 id="ourservice">Our Services</h2>
+    <div id="row1">
+        <div id="service">
+            <h3>Blinds Installation</h3>
+            <p id="list">
+                Roller Blinds<br>
+                Wooden Blinds<br>
+                Vertical Blinds<br>
+                Jute and Reed Blinds<br>
+                Shutter Awning Blinds<br>
+            </p>
+        </div>
+        <div id="service">
+            <h3>Shadeports</h3>
+            <p id="list">
+                Wall Mounted<br>
+                Standard 4-Pole<br>
+                Two Columns-Cantilever<br>
+                Four Columns-Semi Cantilever<br>
+            </p>
+        </div>
+        <div id="service">
+            <h3>Carports</h3>
+            <p id="list">
+                Flat Roof Carports<br>
+                Gable Roof Carports<br>
+                DIY & Custom Carports<br>
+                Flemish Gable Carports<br>
+                Timber Carports Frame<br>
+            </p>
+        </div>
+    </div>
+    <br><br><br><br>
+
+    <footer id="footer">
+        <div id="row">
+            <div class="col">
+                <h2>Address</h2>
+                <p>204 Thomas Podile Road</p>
+                <p>Tembisa, Khatamping Section</p>
+                <p>1632</p>
+            </div>
+
+            <div class="col">
+                <h2>Follow US</h2>
+                <p><a href="https://web.facebook.com/Shaddy-Blinds-company-107065441769825">Facebook</a></p>
+                <p><a href="https://twitter.com/shadrack_aka">Twitter</a></p>
+                <p><a href="https://whatsapp.com/dl/">Whatsapp</a></p>
+                
+            </div>
+
+            <div class="col">
+                <h2>Contact US</h2>
+                <p>Phone: +27 78 973 1952</p>
+                <p>Alternative: +27 65 818 7159</p>
+                <p>Email: <a href="mailto:RammetloaneShadrack@gmail.com">RammetloaneShadrack@gmail.com</a></p>
+            </div>
+            <!--<div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3585.57796350218!2d28.196752414726536!3d-26.014638962610587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e956c0cae601ecd%3A0xc067c893c6b62bc2!2s!5e0!3m2!1sen!2sza!4v1647858500736!5m2!1sen!2sza" 
+                width="200" height="100" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>-->
+        </div>
+
+            <hr class="fotterP">
+            <p id="copyright">Rammetloane Enterprise PTY © <span style="float:right;">Copyright 2022 - All Rights Reserved</span></p>
+    </footer>
+</body>
+</html>
